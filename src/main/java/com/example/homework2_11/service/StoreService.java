6424370b806f2c279904888ec03fc8b7795d7e1d
ChibiaSad <magur1997@gmail.com>
2022-10-08 14:18:28 +0300

    изменил аннотации

--- src/main/java/com/example/homework2_11/service/StoreService.java
+++ src/main/java/com/example/homework2_11/service/StoreService.java
@@ -2,18 +2,20 @@ package com.example.homework2_11.service;
 
 import com.example.homework2_11.entity.ShoppingCart;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.List;
 
 @Service
-@SessionScope
 public class StoreService {
     private final ShoppingCart shoppingCart;
 
-    public StoreService() {
-        this.shoppingCart = new ShoppingCart();
+    public StoreService(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
     }
+
+//    public StoreService(@Qualifier("cart") ShoppingCart shoppingCart) {
+//        this.shoppingCart = shoppingCart;
+//    }
 
     public void addOrder(List<Integer> products) {
         shoppingCart.getProducts().addAll(products);
